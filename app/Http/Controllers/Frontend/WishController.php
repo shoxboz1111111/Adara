e3a1e2efc6c8bc52c9548a58988c0a72f0dd5141
shoxboz1111111 <sherzodbekqurbonov368@gmail.com>
2024-04-13 15:58:30 +0500

    first commit

---
+++ app/Http/Controllers/Frontend/WishController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\frontend;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\Wish;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+class WishController extends Controller
+{
+
+    public function addWish(Request $request, $product_id)
+    {
+        $client = Session::get('client');
+        if ($client) {
+            $product = Product::where('id', $product_id)->first();
+
+
+            if ($product) {
+                Wish::create([
+                    'client_id' => $client->id,
+                    'product_id' => $product_id,
+                    'price' => $product->price,
+                    'name' => $product->name,
+                    'image' => $product->image,
+                    'discount' => $product->discount,
+                    'count' => $product->caunt,
+                ]);
+                $notification = array(
+                    'message' => 'Added Wish successfully!',
+                    'alert-type' => 'success'
+                );
+            }else{
+                $notification = array(
+                    'message' => 'Not add!',
+                    'alert-type' => 'success'
+                );
+            }
+        } else {
+            $notification = array(
+                'message' => 'You are not log inned!',
+                'alert-type' => 'warning'
+            );
+        }
+
+        return redirect()->back()->with($notification);
+    }
+    public function deleteWish($id)
+    {
+        $client = session('client');
+        $wish = Wish::where('id', $id)->first();
+        if ($client && $wish) {
+            $wish->delete();
+            $notification = array(
+                'message' => 'Delete successfully!',
+                'alert-type' => 'warning'
+            );
+        } else {
+            $notification = array(
+                'message' => 'Delete  successfully!',
+                'alert-type' => 'warning'
+            );
+        }
+        return redirect()->back()->with($notification);
+    }
+}
---
+++ database/migrations/2024_03_09_202440_create_carts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->integer('price')->nullable();
+            $table->string('image')->nullable();
+            $table->integer('discount')->nullable();
+            $table->integer('color_id')->nullable();
+            $table->integer('size_id')->nullable();
+            $table->integer('count')->nullable();
+            $table->integer('client_id')->nullable();
+            $table->string('status')->default('active');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+}
